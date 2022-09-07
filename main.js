135d023e228c83b3117afd4873d3b7dfb0ae359f
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-07 11:22:40 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
-    // Área de la circunferencia = PI * radio˄2
 
+/// calcular la velocida de un proyectil ///
 
-    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
+let kim = 2
 
+let min = 5
 
-    console.group(`%cFormula ${2} * ${Math.PI} * ${radio}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+let division = ( kim / min )
 
-        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
-    console.groupEnd();
+console.log( `La formula es:  ${ division } = ${ kim } / ${ min }`)
 
-    console.group(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
-    console.groupEnd();
+console.log( `La velocidad del proyectil es: " ${division }"`)
+
 })
