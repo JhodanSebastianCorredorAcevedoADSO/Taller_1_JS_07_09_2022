8dd47e7bc29162bbe9a7870769e20fbac6f3efb1
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-11 12:45:26 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-/// calcular el area y volumen de un cilindro
-/// operacion basica *,+
+/// calcular en volumen de un elipsoide
+/// operacion basica +,/
     
 /// import math
     
+let v=4/3
+    
 let pi=3.14
     
-let ra=2
+let a=11
     
-let h=3
+let b=12
     
-let l=2
+let c=13
 
-/// resultado y operacion
-    
-console.log( `%cFormula es: ${R1} = ${ l } * ${pi*ra*ra}) + ${l*pi+ra} * ${h}`)
-console.log( `%cFormula es: ${R2} = ${pi*ra*ra} * ${h}`)
-    
-/// resultado y operacion
+/// mltiplicar los datos integrados
 
-console.log("resultado ", ${R1})
-console.log("resultado ", ${R2})
+console.log(`%cFormula es: ${mul} = ${v*pi*a*b*c}`)
+    
+/// resulatdo y operacion
+console.log("resultado", ${mult})
 
 })
