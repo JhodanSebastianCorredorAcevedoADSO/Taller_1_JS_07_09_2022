7cb361b4e181532c8d433f8fc4637bfe05518dca
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-11 12:16:47 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-/// calcular el volumen de una esfera ///
+/// calcule la siguiente expresion 
+/// operacion basica +,-,*,/
+let a=3
 
-let radio1 = 3
+let b=3
 
-let volumen = (4/2)
+let c=3
 
-let pi = 3.14
+console.log( `la formiula es: ${R} = ${(a+7*c)} / ${(b+2-a)} + ${2} * ${b}  `)
 
-let radio2 = 23
+/// resultado u operacion
 
-let esfera1 = radio1 * volumen
-
-let esfera2 = volumen * pi * radio2
-
-console.log(` La primera formula es: ${ radio1 } * ${ volumen }` )
-
-console.log(` La segunda formula es: "${ volumen } * ${ pi } * ${ radio2}" ` )
-
-console.log(` el volumen de la esfera es: " ${ esfera1 } " `)
-
-console.log(` el volumen de la esfera es: " ${ esfera2 } " `)
-
+console.log("resultdo", R)
 
 })
