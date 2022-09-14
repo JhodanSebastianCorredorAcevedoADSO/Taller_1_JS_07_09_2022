782b48d47b3a38f73fe865df836e6a2b6a41c343
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-14 09:58:21 -0500

    Ejercicio 25

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
 
-/// calcular cuanto dinero tenia el jefe de obra 
-/// operacion basica +,*
+/// calcular La cantidad de gramos de harina para hacer el bizcocho
+/// operacion basica /
     
-let llav=11500
+let hari=100
     
-let bomb=1168000
+let caca=10
     
-let per=87000
+let div=(hari/20)
     
-let sobr=91000
+/// resultado y operacion
     
-let mul1=(llav*5)
-    
-let mul2=(per*3)
-    
-let sum=(mul1+mul2+bomb)
-    
-let sum2=(sobr+sum)
-    
-/// resultado y proceso
-    
-console.log(`valor de las llaves:", ${mul1}`)
-
-console.log(`valor de los pernos:, ${mul2}`)
- 
-console.log(`suma de los productos:, ${sum}`)
-
-console.log(`cuanto dinero tenia:, ${sum2}`)
+console.log(`cantida de harina:, ${div}`)
 
 })
